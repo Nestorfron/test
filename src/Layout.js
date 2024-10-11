117f028eadd69ee61740f270ac1d2286d90ca89b
Nestorfron <nestorfrones07@hotmail.com>
2024-10-11 17:51:38 -0300

    theme

--- src/Layout.js
+++ src/Layout.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import '../src/front/styles/App.css';
+import React from "react";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import "../src/front/styles/App.css";
 import NavBar from "../src/front/components/navbar";
-import {BrowserRouter , Routes, Route} from "react-router-dom";
-import Dashboard from './front/pages/dashboard';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./front/pages/dashboard";
 
-
-
-function Layout() {
-
-
+function Layout({ Component, pageProps }) {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/users" element={<h1>Users</h1>} />
-        <Route path="/assets" element={<h1>Assets</h1>} />
-        <Route element={<h1>Not found!</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        <BrowserRouter {...pageProps}>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/users" element={<h1>Users</h1>} />
+            <Route path="/assets" element={<h1>Assets</h1>} />
+            <Route element={<h1>Not found!</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </NextThemesProvider>
+    </NextUIProvider>
   );
 }
 
--- src/front/pages/dashboard.jsx
+++ src/front/pages/dashboard.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { useTheme } from "next-themes";
 import "../styles/index.css";
+import { Button } from "@nextui-org/react";
 
 const Dashboard = () => {
-   
+  const { theme, setTheme } = useTheme();
 
   return (
-    <div>Hola Mundo</div>
+    <div>
+      The current theme is: {theme}
+      <Button onClick={() => setTheme("light")}>Light Mode</Button>
+      <Button onClick={() => setTheme("dark")}>Dark Mode</Button>
+    </div>
   );
 };
 
